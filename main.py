7083a99d0ee48a5b7e542f2cd1adbcab4eafc3b5
Angela-Haim <hei.angela.5@gmail.com>
2025-07-24 09:41:33 +0300

    Contenu de l'examen

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue dans mon API FastAPI"}
+@app.get("/hello")
+def read_hello(request: Request):
+    accept_headers = request.headers.get("Accept")
+    return JSONResponse({"message": "Hello World"}, status_code=200)
+
+
+class WelcomeRequest(BaseModel):
+    name: str
+
+
+@app.get("/welcome")
+def welcome_user(request: WelcomeRequest):
+    return {f"Bienvenue {request.name}"}
+
+class StudentsRequest(BaseModel):
+    reference: str
+    firstName: str
+    lastName: str
+    age: int
+
+@app.post("/students")
+def post_students(request: Request, request_body: StudentsRequest):
+    created_headers = request.headers.get("Created")
+        return JSONResponse({f"Reference {request_body.reference}",
+                             f"FirstName {request_body.firstname}",
+                             f"LastName {request_body.lastName}",
+                             f"Age {request_body.age}"},
+                            status_code=200
+                             )
